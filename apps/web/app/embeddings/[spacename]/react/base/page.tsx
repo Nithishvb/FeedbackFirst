2a7110f4fd16031100b7f4db5218aa8f5dfabc9b
Nithish <nithishmohandass06@gmail.com>
2024-07-27 20:34:45 +0530

    feat: added dynamic conntent to the testimonial section

--- apps/web/app/embeddings/[spacename]/react/base/page.tsx
+++ apps/web/app/embeddings/[spacename]/react/base/page.tsx
@@ -1,14 +1,26 @@
+"use client";
+
+import { distributeIntoSubArrays } from "@/lib/helpers/distributeFeedbacks";
+import useFetch from "@/lib/hooks/useFetch";
 import Image from "next/image";
+import { useParams } from "next/navigation";
 import React from "react";
 
 const page = () => {
+  const params = useParams<{ spacename: string }>();
+  const { data } = useFetch(
+    `${process.env.NEXT_PUBLIC_URL}/api/feedbacks/text/${params.spacename}`
+  );
+  const result = distributeIntoSubArrays(data);
+  console.log(result);
+
   return (
     <div>
       <div className="min-w-screen min-h-screen bg-gray-50 flex items-center justify-center">
         <div className="w-full bg-gradient-to-br from-purple-700 to-violet-900 border-t border-b border-gray-200 px-5 py-16 md:py-12 text-gray-800">
           <div className="w-full max-w-6xl mx-auto">
             <div className="text-center max-w-xl mx-auto">
-            <h1 className="text-6xl md:text-7xl font-bold mb-5 text-white">
+              <h1 className="text-6xl md:text-7xl font-bold mb-5 text-white">
                 What people <br />
                 are saying.
               </h1>
@@ -21,70 +33,74 @@ const page = () => {
               </div>
             </div>
             <div className="-mx-3 md:flex items-start">
-              <div className="px-3 md:w-1/3">
-                <div className="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
-                  <div className="w-full flex mb-4 items-center">
-                    <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
-                      <Image
-                        src=""
-                        alt=""
-                        height={200}
-                        width={200}
-                      />
+              {result.map((feed) => (
+                <div className="px-3 md:w-1/3">
+                  {feed.map((e, index: number) => (
+                    <div
+                      key={index}
+                      className="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6"
+                    >
+                      <div className="w-full flex mb-4 items-center">
+                        <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                          <Image
+                            src=""
+                            alt=""
+                            height={200}
+                            width={200}
+                          />
+                        </div>
+                        <div className="flex-grow pl-3">
+                          <h6 className="font-bold text-sm uppercase text-gray-600">
+                            {e.name}
+                          </h6>
+                        </div>
+                      </div>
+                      <div className="w-full">
+                        <p className="text-sm leading-tight">
+                          <span className="text-lg leading-none italic font-bold text-gray-400 mr-1">
+                            "
+                          </span>
+                          {e.feedback}
+                          <span className="text-lg leading-none italic font-bold text-gray-400 ml-1">
+                            "
+                          </span>
+                        </p>
+                      </div>
                     </div>
-                    <div className="flex-grow pl-3">
-                      <h6 className="font-bold text-sm uppercase text-gray-600">
-                        Kenzie Edgar.
-                      </h6>
-                    </div>
-                  </div>
-                  <div className="w-full">
-                    <p className="text-sm leading-tight">
-                      <span className="text-lg leading-none italic font-bold text-gray-400 mr-1">
-                        "
-                      </span>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Quos sunt ratione dolor exercitationem minima quas itaque
-                      saepe quasi architecto vel! Accusantium, vero sint
-                      recusandae cum tempora nemo commodi soluta deleniti.
-                      <span className="text-lg leading-none italic font-bold text-gray-400 ml-1">
-                        "
-                      </span>
-                    </p>
-                  </div>
+                  ))}
+                  {/* <div className="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
+                      <div className="w-full flex mb-4 items-center">
+                        <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                          <Image
+                            src=""
+                            alt=""
+                            height={200}
+                            width={200}
+                          />
+                        </div>
+                        <div className="flex-grow pl-3">
+                          <h6 className="font-bold text-sm uppercase text-gray-600">
+                            Stevie Tifft.
+                          </h6>
+                        </div>
+                      </div>
+                      <div className="w-full">
+                        <p className="text-sm leading-tight">
+                          <span className="text-lg leading-none italic font-bold text-gray-400 mr-1">
+                            "
+                          </span>
+                          Lorem ipsum, dolor sit amet, consectetur adipisicing elit.
+                          Dolore quod necessitatibus, labore sapiente, est,
+                          dignissimos ullam error ipsam sint quam tempora vel.
+                          <span className="text-lg leading-none italic font-bold text-gray-400 ml-1">
+                            "
+                          </span>
+                        </p>
+                      </div>
+                    </div> */}
                 </div>
-                <div className="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
-                  <div className="w-full flex mb-4 items-center">
-                    <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
-                      <Image
-                        src=""
-                        alt=""
-                        height={200}
-                        width={200}
-                      />
-                    </div>
-                    <div className="flex-grow pl-3">
-                      <h6 className="font-bold text-sm uppercase text-gray-600">
-                        Stevie Tifft.
-                      </h6>
-                    </div>
-                  </div>
-                  <div className="w-full">
-                    <p className="text-sm leading-tight">
-                      <span className="text-lg leading-none italic font-bold text-gray-400 mr-1">
-                        "
-                      </span>
-                      Lorem ipsum, dolor sit amet, consectetur adipisicing elit.
-                      Dolore quod necessitatibus, labore sapiente, est,
-                      dignissimos ullam error ipsam sint quam tempora vel.
-                      <span className="text-lg leading-none italic font-bold text-gray-400 ml-1">
-                        "
-                      </span>
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="px-3 md:w-1/3">
+              ))}
+              {/* <div className="px-3 md:w-1/3">
                 <div className="w-full mx-auto rounded-lg bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
                   <div className="w-full flex mb-4 items-center">
                     <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
@@ -273,7 +289,7 @@ const page = () => {
                     </p>
                   </div>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
